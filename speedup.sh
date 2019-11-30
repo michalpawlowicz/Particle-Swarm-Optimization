c62062500355b9af570f487c5df7897980484bd9
Michał Pawłowicz <michal.pawlowicz@yahoo.com>
2019-11-30 15:51:19 +0100

    fix

--- speedup.sh
+++ speedup.sh
@@ -3,13 +3,13 @@
 for i in `seq 1 $(($(grep -c ^processor /proc/cpuinfo)))`
 do
         TIMEFORMAT=%R
-        echo $run"th for "$i" cpus"
-        printf "run: %d cpus: %d time: %f\n" $run $i $( { time ./run.sh $i > /dev/null 2>&1; } 2>&1 )
+        printf "cpus: %d time: %f\n" $i $( { time ./run.sh $i > /dev/null 2>&1; } 2>&1 )
 done
+
+echo "AKKA START"
 
 for i in `seq 1 $(($(grep -c ^processor /proc/cpuinfo)))`
 do
         TIMEFORMAT=%R
-        echo "akka "$run"th for "$i" cpus"
-        printf "run: %d cpus: %d time: %f\n" $run $i $( { time python run_akka.py $i > /dev/null 2>&1; } 2>&1 )
+        printf "cpus: %d time: %f\n" $i $( { time python run_akka.py $i > /dev/null 2>&1; } 2>&1 )
 done
