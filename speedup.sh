866b29ea2cdcc8f9c58e3f062e15940ba631748c
Michał Pawłowicz <michal.pawlowicz@yahoo.com>
2019-12-01 22:34:11 +0100

    wip

--- speedup.sh
+++ speedup.sh
@@ -5,11 +5,3 @@ do
         TIMEFORMAT=%R
         printf "cpus: %d time: %f\n" $i $( { time ./run.sh $i > /dev/null 2>&1; } 2>&1 )
 done
-
-echo "AKKA START"
-
-for i in `seq 1 $(($(grep -c ^processor /proc/cpuinfo)))`
-do
-        TIMEFORMAT=%R
-        printf "cpus: %d time: %f\n" $i $( { time python run_akka.py $i > /dev/null 2>&1; } 2>&1 )
-done
