4750467dd0b73f9146fa49785bd97fdc81c65f13
Michał Pawłowicz <michal.pawlowicz@yahoo.com>
2019-12-01 22:25:45 +0100

    Update speedup_akka.sh

--- speedup_akka.sh
+++ speedup_akka.sh
@@ -3,6 +3,6 @@ echo "AKKA"
 for i in `seq 1 $(($(grep -c ^processor /proc/cpuinfo)))`
 do
         TIMEFORMAT=%R
-        TIME=$(time (python run_akka.py $i >/dev/null 2>&1) 2>&1)
+        TIME=$(time (python3 run_akka.py $i >/dev/null 2>&1) 2>&1)
         printf "cpus: %d time: %f\n" $i $TIME
 done
