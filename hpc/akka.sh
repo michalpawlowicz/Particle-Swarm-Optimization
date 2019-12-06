f9d0e5601fc5e9e66be6250dd40c0c1cec1add5d
Michał Pawłowicz <michal.pawlowicz@yahoo.com>
2019-12-06 14:34:54 +0100

    fix

--- hpc/akka.sh
+++ hpc/akka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "Akka implementation for %d cpus with %s configuration, run=%d\n" $SLURM_JOB_CPUS_PER_NODE $CONFIG $RUN
+printf "> Akka implementation for %d cpus with %s configuration, run=%d\n" $SLURM_JOB_CPUS_PER_NODE $CONFIG $RUN
 
 TIMEFORMAT=%R
 time (cd .. && ./run_akka.sh $CONFIG)
