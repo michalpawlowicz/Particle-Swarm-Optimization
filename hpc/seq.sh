8af19d96beea9bfd932ab05d9a7bb07dc6dfa453
Michał Pawłowicz <michal.pawlowicz@yahoo.com>
2019-12-06 02:12:39 +0100

    fix

--- hpc/seq.sh
+++ hpc/seq.sh
@@ -3,4 +3,4 @@
 printf "seq impl for %d cpus\n" $SLURM_JOB_CPUS_PER_NODE
 
 TIMEFORMAT=%R
-printf "time: %f\n" $( { time cd .. && ./run.sh $SLURM_JOB_CPUS_PER_NODE > /dev/null 2>&1; } 2>&1 )
+printf "time: %f\n" $( { time (cd .. && ./run.sh $SLURM_JOB_CPUS_PER_NODE > /dev/null 2>&1); } 2>&1 )
