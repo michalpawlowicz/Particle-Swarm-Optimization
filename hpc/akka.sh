36b44dcb02028f10f96308dc6316ada09f071f73
Michał Pawłowicz <michal.pawlowicz@yahoo.com>
2019-12-06 15:00:56 +0100

    fix

--- hpc/akka.sh
+++ hpc/akka.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
 printf "> Akka implementation for %d cpus with %s configuration, run=%d\n" $SLURM_JOB_CPUS_PER_NODE $CONFIG $RUN
-
-TIMEFORMAT=%R
 time (cd .. && ./run_akka.sh $CONFIG)
--- hpc/seq.sh
+++ hpc/seq.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
 printf "> Seq implementation for %d cpus with %s configuration, run=%d\n" $SLURM_JOB_CPUS_PER_NODE $CONFIG $RUN
-
-TIMEFORMAT=%R
 time (cd .. && ./run.sh $SLURM_JOB_CPUS_PER_NODE $CONFIG)
