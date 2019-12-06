9b331043b434e2fefd4884560f1e7190b525b1f1
Michał Pawłowicz <michal.pawlowicz@yahoo.com>
2019-12-06 01:48:54 +0100

    endl

--- hpc/akka.sh
+++ hpc/akka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "akka impl for %d cpus" $SLURM_JOB_CPUS_PER_NODE
+printf "akka impl for %d cpus\n" $SLURM_JOB_CPUS_PER_NODE
 
 TIMEFORMAT=%R
 TIME=$(time (../run_akka.sh 1>/dev/null 2>&1) 2>&1)
--- hpc/seq.sh
+++ hpc/seq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "seq impl for %d cpus" $SLURM_JOB_CPUS_PER_NODE
+printf "seq impl for %d cpus\n" $SLURM_JOB_CPUS_PER_NODE
 
 TIMEFORMAT=%R
 printf "time: %f\n" $( { time ../run.sh $SLURM_JOB_CPUS_PER_NODE > /dev/null 2>&1; } 2>&1 )
