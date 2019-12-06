b212260cd17d5dc7b3bd014e01f9729586c4b743
Michał Pawłowicz <michal.pawlowicz@yahoo.com>
2019-12-06 01:53:19 +0100

    fix

--- hpc/akka.sh
+++ hpc/akka.sh
@@ -3,5 +3,5 @@
 printf "akka impl for %d cpus\n" $SLURM_JOB_CPUS_PER_NODE
 
 TIMEFORMAT=%R
-TIME=$(time (../run_akka.sh 1>/dev/null 2>&1) 2>&1)
+TIME=$(time (cd .. && ./run_akka.sh 1>/dev/null 2>&1) 2>&1)
 printf "time: %f\n" $TIME
