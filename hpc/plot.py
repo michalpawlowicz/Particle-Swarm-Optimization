0322a67915fccf5cc3d40a852a8028cf3feafc5c
Michał Pawłowicz <michal.pawlowicz@yahoo.com>
2019-12-08 21:44:37 +0100

    fix

--- hpc/plot.py
+++ hpc/plot.py
@@ -3,7 +3,7 @@ import os
 from collections import defaultdict
 import matplotlib.pyplot as plt
 
-benchmarks=["benchmarks/d50p256"]
+benchmarks=["benchmarks/d50p128"]
 
 #error-zeus-4-akka.sh-d512p64-8.out
 #user\t3m40.341s\n', 'sys\t0m10.272s\n']
@@ -21,18 +21,14 @@ for dirp in benchmarks:
             with open(dirp + "/" + filename, "r") as fp:
                 lines = fp.readlines()
             for l1, l2, l3 in zip(lines, lines[1:], lines[2:]):
-                m_user = re.match("user.*(\d+)m(\d+\.\d+)", l2)
-                m_sys = re.match("sys.*(\d+)m(\d+\.\d+)", l3)
-                if m_user is not None and m_sys is not None:
-                    sys_min, sys_sec = m_sys.groups()
-                    user_min, user_sec = m_user.groups()
-                    min = int(user_min) + int(sys_min)
-                    sec = float(user_sec) + float(sys_sec)
+                m = re.match("real.*(\d+)m(\d+\.\d+)", l2)
+                if m is not None:
+                    real_min, real_sec = m.groups()
                     if impl in d:
-                        d[impl][int(cpus)].append(min * 60 + sec)
+                        d[impl][int(cpus)].append(int(real_min) * 60 + float(real_sec))
                     else:
                         d[impl] = defaultdict(list)
-                        d[impl][int(cpus)].append(min * 60 + sec)
+                        d[impl][int(cpus)].append(int(real_min) * 60 + float(real_sec))
     if len(d) > 2:
         raise RuntimeError("Len is > 2")
 
@@ -46,11 +42,14 @@ for dirp in benchmarks:
             print("WARRNING! Len of seq for {0} cpus equals {1}".format(cpus, len(l)))
 
     # PLOT
-    for impl in [akka, seq]:
+    for (impl, label) in [(akka, "akka"), (seq, "seq")]:
         Y = []
         for (cpus, l) in impl:
             Y.append(sum(l) / len(l))
             print("For {0} cpus avg = {1}".format(cpus, sum(l) / len(l)))
-        plt.plot(range(len(Y)), [Y[0] / y for y in Y])
+        plt.plot(range(len(Y)), [Y[0] / y for y in Y], label=label)
 
+    plt.plot(range(24), [x+1 for x in range(24)])
+
+    plt.legend(loc="upper left")
     plt.show()
