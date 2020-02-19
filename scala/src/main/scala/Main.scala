ed869dc2e14bcab5b87284cfd5f1a3abe7c8d858
Michal <michal.pawlowicz@yahoo.com>
2020-02-20 01:16:32 +0200

    WUP

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -4,10 +4,9 @@ object Main extends App {
   val particlesCount = 32
   val dimension = 128
   val iterMax = 3e5
-  //val fn = (v: Vector[Double]) => 418.9829 * dimension - v.reduce((acc, vi) => acc + vi * scala.math.sin(scala.math.sqrt(scala.math.abs(vi))))
   val fn = (v: Vector[Double]) => 418.9829 * dimension - v.map(vi => vi * scala.math.sin(scala.math.sqrt(scala.math.abs(vi)))).sum
   val endCondition = (iteration : Int, fitness : Double) => iteration > iterMax
-  val domain : Domain = new Domain(-450, 450)
+  val domain : Domain = new Domain(-500, 500)
 
   val omegaMax : Double = 1.8
   val omegaMin : Double = 0.1
