750cb1cee9f6e3b2bc6a97f19ef4d1d5580a701b
Michal Pawlowicz <michal.pawlowicz@yahoo.com>
2020-02-20 15:19:52 -0800

    prints

--- scala/run_configurations.py
+++ scala/run_configurations.py
@@ -9,7 +9,9 @@ if not os.path.isdir(outdir):
 
 for config in os.listdir(directory):
     if config.endswith(".properties"):
+        print("-> Running configuration {}".format(config))
         for cpus in range(cpusmax):
+            print("    -> configuration run on {} cpus".format(cpus))
             configname = config.split('.')[0]
             os.system("python3 run.py {0} {1} {2}".format(cpus, directory + config, "{0}/{1}_{2}".format(outdir, configname, cpus)))
     else:
