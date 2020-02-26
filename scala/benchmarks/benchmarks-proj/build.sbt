6a1734d59e7f2000c4c31dd81a43f844f341ea31
Michal <michal.pawlowicz@yahoo.com>
2020-02-27 01:15:44 +0200

    benchmarks

--- scala/benchmarks/benchmarks-proj/build.sbt
+++ scala/benchmarks/benchmarks-proj/build.sbt
@@ -3,3 +3,7 @@ name := "benchmarks-proj"
 version := "0.1"
 
 scalaVersion := "2.13.1"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.6.3"
+libraryDependencies += "com.typesafe" % "config" % "1.3.3"
+
--- scala/benchmarks/benchmarks-proj/src/main/scala/Main.scala
+++ scala/benchmarks/benchmarks-proj/src/main/scala/Main.scala
@@ -5,12 +5,8 @@ import com.typesafe.config.ConfigFactory
 import mapso.{Domain, Parameters, SwarmActor}
 
 object Main extends App {
-
   val fileName = System.getProperty("confAppName")
-
-  println("Loaded configuration: " +  fileName)
   val config = ConfigFactory.parseFile(new File(fileName))
-
   val graph = config.getString("graph")
   val particlesCount=config.getString("particlesCount").toInt
   val dimension=config.getString("dimension").toInt
@@ -19,13 +15,10 @@ object Main extends App {
   val omegaMax=config.getString("omegaMax").toDouble
   val phi_1=config.getString("phi_1").toDouble
   val phi_2=config.getString("phi_2").toDouble
-
   val fn = (v: Vector[Double]) => 418.9829 * dimension - v.map(vi => vi * scala.math.sin(scala.math.sqrt(scala.math.abs(vi)))).sum
   val endCondition = (iteration : Int, fitness : Double) => iteration > iterMax
   val domain : Domain = new Domain(-500, 500)
-
   val parameters : Parameters = new Parameters(omegaMin, omegaMax, phi_1, phi_2, (omegaMax - omegaMin) / iterMax)
-
   val system = ActorSystem("HelloSystem")
   val swarm = system.actorOf(Props(new SwarmActor(GraphParser.parse(graph), particlesCount, fn, endCondition, domain, parameters, dimension)))
 }
